939605604878f9396bdc7ee3e2c601eb46339e01
abhisiddanthi <abhiramsiddanthi@gmail.com>
2025-05-25 14:00:09 +0000

    first init

---
+++ main.js
@@ -0,0 +1,32 @@
+// Your image sources
+const images = [
+    'Images/Clientele/clientele1.png',
+    'Images/Clientele/clientele2.png',
+    'Images/Clientele/clientele3.png',
+    'Images/Clientele/clientele4.png',
+    'Images/Clientele/clientele5.png',
+    'Images/Clientele/clientele6.png',
+    'Images/Clientele/clientele7.png',
+    'Images/Clientele/clientele8.png',
+    'Images/Clientele/clientele9.png',
+    'Images/Clientele/clientele10.png',
+    'Images/Clientele/clientele11.png',
+    'Images/Clientele/clientele12.png',
+    'Images/Clientele/clientele13.png',
+    'Images/Clientele/clientele14.png',
+
+];
+
+const track = document.getElementById('carousel-track');
+
+function appendImages(imgArray) {
+  imgArray.forEach(src => {
+    const img = document.createElement('img');
+    img.src = src;
+    img.alt = "carousel image";
+    track.appendChild(img);
+  });
+}
+
+appendImages(images);
+appendImages(images);
